8269c9345f32a1dbf4839e6403e597fce368146e
Yanik Nayee <yaniknayee123@gmail.com>
2023-03-05 10:06:48 -0500

    Created the app header

--- delivery-app-clone/screens/HomeScreen.js
+++ delivery-app-clone/screens/HomeScreen.js
@@ -1,6 +1,13 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React, { useLayoutEffect } from 'react'
-import { useNavigation } from '@react-navigation/native'
+import { View, Text, SafeAreaView, Image, TextInput, ScrollView} from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+    UserIcon,
+    ChevronDownIcon,
+    MagnifyingGlassIcon,
+    AdjustmentsIcon,
+    AdjustmentsVerticalIcon,
+}   from "react-native-heroicons/outline"; 
 
 const HomeScreen = () => {
     const navigation = useNavigation();
@@ -13,10 +20,48 @@ const HomeScreen = () => {
     }, []);
 
   return (
-    <SafeAreaView>
-      <Text className='text-red-500'>HomeScreen</Text>
-    </SafeAreaView>
-  )
-}
+    <SafeAreaView className="bg-white pt-4">
 
-export default HomeScreen
+        {/*header*/}
+
+        <View className="flex-row pb-3 items-center mx-4 space-x-2 px-3 ">
+            <Image
+                source={{
+                    uri: "https://links.papareact.com/wru",
+                }}
+                className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+            />
+
+            <View className="flex-1">
+                <Text className="font-bold text-gray-400 text-xs">
+                    Deliver Now!
+                </Text>
+                <Text className="font-bold text-xl">Current Location!
+                    <ChevronDownIcon size={20} color="#00CCBB" />
+                </Text>
+            </View>
+            <UserIcon size={35} color="#00CCBB" />
+        </View>
+
+        {/*search*/}
+        <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+            <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+                <MagnifyingGlassIcon color="gray" size={20} />
+                <TextInput 
+                placeholder="Restaurants and cuisines"
+                keyboardType="default"
+                />
+            </View>
+            <AdjustmentsVerticalIcon color="#00CCBB" />
+        </View>
+        
+        {/*body*/}
+        <ScrollView>
+            {/*Categories*/}
+        </ScrollView>
+
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
--- delivery-app-clone/tailwind.config.js
+++ delivery-app-clone/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
             "./App.{js,jsx,ts,tsx}", 
             "./screens/**/*.{js,jsx,ts,tsx}",
+            "./HomeScreen/**/*.{js,jsx,ts,tsx}",
             "./pages/**/*.{js,jsx,ts,tsx}",
             "./content/**/*.{js,jsx,ts,tsx}"
           ],
